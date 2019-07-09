c4538f4c0839eaab1b00ae2f7a36e4c9462a4740
dhritisaini <dhritisaini@gmail.com>
2019-07-09 17:20:57 +0530

    Updated code

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = '7f868759057958b14d94e62263bfae99';
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
@@ -8,4 +13,30 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+// app.post('/', function (req, res) {
+//   res.render('index');
+//   console.log(req.body.city);
+// })
+
+// const request = require('request');
+// const apiKey = '*****************';
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
