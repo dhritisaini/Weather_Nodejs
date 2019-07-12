68e5735ce4a61dcd13c1edddb18e1ca5683496d4
dhritisaini <dhritisaini@gmail.com>
2019-07-12 21:50:50 +0530

    Updated code

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const app = express()
+
 const apiKey = '7f868759057958b14d94e62263bfae99';
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-// app.post('/', function (req, res) {
-//   res.render('index');
-//   console.log(req.body.city);
-// })
-
-// const request = require('request');
-// const apiKey = '*****************';
 
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-request(url, function (err, response, body) {
+
+  request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
@@ -39,4 +30,8 @@ request(url, function (err, response, body) {
       }
     }
   });
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!')
 })
